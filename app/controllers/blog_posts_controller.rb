14690647fc38ced6a8ee415f9a872ade86ede3f4
Zerxe <amc92v@gmail.com>
2024-03-08 07:17:52 +0000

    10- Adding a BlogPost Destroy Action & Refactoring

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,10 @@
 class BlogPostsController < ApplicationController
+    before_action :set_blog_post, except: [:index, :new, :create] #only: [:show, :edit, :update, :destroy]
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-         @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
@@ -23,21 +23,30 @@ class BlogPostsController < ApplicationController
     end
     
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post
         else
             render :edit, status: :unprocessable_entity
         end
     end
+
+    def destroy        
+        @blog_post.destroy
+        redirect_to root_path
+    end
     
     private
 
     def blog_post_params
-      params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
     end
 end
