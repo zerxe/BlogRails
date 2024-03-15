9bea5234f2b5ab4ee2febd658234ff0039cf6c9b
Zerxe <amc92v@gmail.com>
2024-03-15 11:36:41 +0000

    Modificado init tinymce

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require tinymce/tinymce.min
+document.addEventListener("DOMContentLoaded", function() {
+    tinymce.init({
+        selector: 'textarea.tinymce',
+        promotion: false
+    });
+});
 
 
