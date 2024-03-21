213f566b69ba6954ea1e09d053461d9aedef424b
Zerxe <amc92v@gmail.com>
2024-03-21 10:02:12 +0000

    How to Upgrade Ruby Versions (3.3.0 asdf)

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module Blog
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exist?(env_file)
     end
   end
 end
